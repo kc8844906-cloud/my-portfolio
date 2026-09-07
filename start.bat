a234e9d624cf6ae4bccd49d4907abce2371df548
Virander Kumar <KC8844906@gmail.com>
2026-09-08 00:10:01 +0530

    Replace fb, insta, and linkedin with Twitter/X button

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ===================================================
-echo   Responsive Portfolio Website - Tailor Lyborm
-echo ===================================================
-echo.
-echo Opening portfolio website in your default browser...
-start "" "%~dp0index.html"
-echo Done! You can now view and edit your portfolio.
-pause
